6ae178f68b38242aa3efc9adb1c2a1d602f8321f
Cam Boucher <cam.boucher.28@gmail.com>
2025-01-26 14:32:43 -0500

    write league analysis logic for api

--- src/routes/leagues.js
+++ src/routes/leagues.js
@@ -1,25 +1,16 @@
 const express = require("express");
+const { analyzeLeague } = require("../teamAnalysis");
 const router = express.Router();
 
-const fetchLeague = async (leagueId) => {
-  const league = await fetch(`https://api.sleeper.app/v1/league/${leagueId}`)
-    .then((data) => data.json())
-    .catch((e) => e);
-  return league;
-};
-
-router.get('/:leagueId', async (req, res) => {
+router.get("/:leagueId", async (req, res) => {
   try {
-    const { leagueId } = req.params; 
-    const leagueData = await fetchLeague(leagueId);
-    
-    if (Object.keys(leagueData).length) {
-      return res.status(200).json({ leagueId });
-    }
-    return res.status(404).json({ message: "Couldn't fetch league data" });
+    const { leagueId } = req.params;
+    const leagueAnalysis = await analyzeLeague(leagueId);
+
+    return res.status(200).json({ leagueAnalysis });
   } catch (error) {
-    console.error('Route handler error:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    console.error("Route handler error:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
--- src/teamAnalysis.js
+++ src/teamAnalysis.js
@@ -1,41 +1,214 @@
+// const leagueId = "1098669917550878720";
+
+const PLAYER_ACQUISITION_TYPES = {
+  add: "add",
+  trade: "trade",
+  draft: "draft",
+};
+
+const EMPTY_TEAM_OBJ = {
+  userId: "",
+  rosterId: "",
+  players: {
+    /*
+      "id": "add" | "trade" | "draft"
+      */
+  },
+  points: {
+    adds: 0,
+    trades: 0,
+    draft: 0,
+  },
+  totalTransactions: 0,
+  cumulativeWins: 0,
+  actualWins: 0,
+  messagesSent: 0,
+};
+
+const getTransactionAndMatchupsByWeek = async (week) => {
+  const transactions = await fetch(
+    `https://api.sleeper.app/v1/league/${leagueId}/transactions/${week}`
+  ).then((res) => res.json());
+  const matchups = await fetch(
+    `https://api.sleeper.app/v1/league/${leagueId}/matchups/${week}`
+  ).then((res) => res.json());
+
+  return { transction, matchups };
+};
+
+export const analyzeLeague = async (leagueId) => {
+  const league = await fetch(
+    `https://api.sleeper.app/v1/league/${leagueId}`
+  ).then((res) => res.json());
+
+  const { draft_id, total_rosters, metadata, settings } = league;
+  const draftId = draft_id;
+  //   const winningRosterId = metadata.latest_league_winner_roster_id;
+  const totalRosters = total_rosters;
+  const firstWeek = settings.start_week;
+  const regularSeasonLength = settings.playoff_week_start - firstWeek;
+
+  // let scoringType = "standard";
+  // if (league.settings.rec === "0.5") {
+  //   scoringType = "half_ppr";
+  // } else if (league.settings.rec === "1") {
+  //   scoringType = "ppr";
+  // }
+
+  const teamsDict = {};
+
+  // create team obj for each roster
+  await fetch(`https://api.sleeper.app/v1/league/${leagueId}/rosters`)
+    .then((res) => res.json())
+    .then((rosters) => {
+      rosters.forEach((roster) => {
+        const { wins, total_moves } = roster.settings;
+        const { roster_id, owner_id } = roster;
+        teamsDict[roster_id] = {
+          ...EMPTY_TEAM_OBJ,
+          userId: owner_id,
+          rosterId: roster_id,
+          actualWins: wins,
+          totalTransactions: total_moves,
+        };
+      });
+    });
+
+  // fetch draftpicks
+  await fetch(`https://api.sleeper.app/v1/draft/${draftId}/picks`)
+    .then((res) => res.json())
+    .then((draftPicks) => {
+      draftPicks.forEach((pick) => {
+        // maybe we can go back later and edit this to calculate points by round and look at + / - points by round
+        const { roster_id, player_id } = pick;
+        teamsDict[roster_id].players[player_id] =
+          PLAYER_ACQUISITION_TYPES.draft;
+      });
+    });
+
+  // instanstiate array to track transactions and matchups by week. start with first week
+  const weeks = new Array(regularSeasonLength);
+
+  const reduceTransactionData = (transactions) => {
+    transactions.forEach((transaction) => {
+      const { status, type, adds, drops } = transaction;
+      if (status === "complete") {
+        const playersAdded = Object.keys(adds);
+        const playersDropped = Object.keys(drops);
+        switch (type) {
+          case "waiver":
+          case "free_agent":
+            playersAdded.forEach((id) => {
+              const rosterId = adds[id];
+              teamsDict[rosterId].players[id] = PLAYER_ACQUISITION_TYPES.add;
+            });
+          case "trade":
+            playersAdded.forEach((id) => {
+              const rosterId = adds[id];
+              teamsDict[rosterId].players[id] = PLAYER_ACQUISITION_TYPES.trade;
+            });
+          default:
+            console.log("transaction type not recognized", type);
+            break;
+        }
+        playersDropped.forEach((id) => {
+          delete teamsDict[rosterId].player_id[id];
+        });
+      }
+    });
+  };
+
+  const reduceMatchupData = (matchups) => {
+    matchups.sort((a, b) => b.points - a.points);
+    matchups.forEach((matchup, i) => {
+      const { roster_id, starters, players_points } = matchup;
+
+      const potentialWins = totalRosters - 1 - i;
+
+      teamsDict[roster_id].cumulativeWins += potentialWins;
+
+      starters.forEach((id) => {
+        const pointsScored = players_points[id];
+        const playerAcquisitionType = teamsDict[roster_id].players[id];
+        switch (playerAcquisitionType) {
+          case PLAYER_ACQUISITION_TYPES.draft:
+            teamsDict[roster_id].points.draft += pointsScored;
+          case playerAcquisitionType.add:
+            teamsDict[roster_id].points.adds += pointsScored;
+          case playerAcquisitionType.draft:
+            teamsDict[roster_id].points.trades += pointsScored;
+          default:
+            console.log(
+              "invalid playerAcquisition type",
+              playerAcquisitionType
+            );
+            break;
+        }
+      });
+    });
+  };
+
+  await weeks.forEach(async (week, i) => {
+    const { transactions, matchups } = await getTransactionAndMatchupsByWeek(
+      i + firstWeek
+    );
+    reduceTransactionData(transactions);
+    reduceMatchupData(matchups);
+  });
+
+  const teamsDictEnum = Object.values(teamsDict);
+  const bestDrafter = [null, 0];
+  const worstDrafter = [null, Infinity];
+  const bestTrader = [null, 0];
+  const worstTrader = [null, Infinity];
+  const bestWaiverWire = [null, 0];
+  const bestMatchupLuck = [null, -1];
+  const worstMatchupLuck = [null, 1];
+
+  teamsDictEnum.forEach((team) => {
+    const { adds, trades, draft } = team.points;
+
+    const theoreticalWinPercentage =
+      team.cumulativeWins / (regularSeasonLength * (totalRosters - 1));
+    const actualWinPercentage = team.actualWins / regularSeasonLength;
+    // high luck factor means team won more games than expected;
+    const luckFactor = actualWinPercentage - theoreticalWinPercentage;
+
+    if (!worstMatchupLuck[0] || worstMatchupLuck[1] < luckFactor) {
+      worstMatchupLuck = [team[rosterid], luckFactor];
+    }
+    if (!bestMatchupLuck[0] || bestMatchupLuck[1] > luckFactor) {
+      bestMatchupLuck = [team[rosterid], luckFactor];
+    }
+    if (!worstDrafter[0] || worstDrafter[1] < draft) {
+      worstDrafter = [team[rosterid], draft];
+    }
+    if (!bestDrafter[0] || bestDrafter[1] > draft) {
+      bestDrafter = [team[rosterid], draft];
+    }
+    if (!worstTrader[0] || worstTrader[1] < trades) {
+      worstTrader = [team[rosterid], trade];
+    }
+    if (!bestTrader[0] || bestTrader[1] > trades) {
+      bestTrader = [team[rosterid], trades];
+    }
+    if (!bestWaiverWire[0] || bestWaiverWire[1] > adds) {
+      bestWaiverWire = [team[rosterid], adds];
+    }
+  });
+
+  return {
+    bestDrafter,
+    worstDrafter,
+    bestTrader,
+    worstTrader,
+    bestWaiverWire,
+    bestMatchupLuck,
+    worstMatchupLuck,
+  };
+};
+
 /*
-fetch league
-    store draftId
-    store metadata.latest_league_winner_roster_id
-fetch draft
-    save metadata.scoring_type
-fetch draftPicks
-    initialize an array of teams. each team is a object (userId / rosterId) --> prob rosterId since that can be matched to matchups
-        draft roster
-        draft points
-        waiver adds property (obj)
-        waiver points
-        trade adds property (obj)
-        trade points
-        messages sent
-        cumulative w / l (play every team every week)
-
-
-go week by week and fetch 
-    transactions
-        for each transaction
-            check if status === complete
-            if type === trade
-                check rosters involved
-                    if roster involved has traded players for that week add palyers to trade adds and reverse for other team
-            if type === waiver || free agent
-                add player to waiver obj
-                delete dropped player from waiver adds fro that team
-    
-    matchups
-        sort matchups by points
-            for each matchup
-                add to wins and losses based on index of matchup
-                for each player
-                    check if drafted / traded / waiver add and add points to appropriate category
-
-
-
                     OTHER //GET https://api.sleeper.app/v1/league/<league_id>/users -- . user.avatar
   // https://sleepercdn.com/avatars/thumbs/${user.avatar}
 */
